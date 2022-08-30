9506e9e840ecc493da7a758ec8402341c8b90061
Priscille Louis-rose <priscille.louisrose@gmail.com>
2022-08-30 12:42:25 +0200

    add uuid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Tweet from "./components/Tweet";
+import Tweet from "./components/Tweet"
 import tweetsArray from './data/tweets'
+import { v4 as uuidv4 } from 'uuid';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {tweetsArray.map(tweet => <Tweet key={tweet.message} tweet={tweet}/>)}
+      {tweetsArray.map(tweet => <Tweet key={uuidv4()} tweet={tweet}/>)}
     </div>
   );
 }
